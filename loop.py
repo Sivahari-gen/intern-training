fff313cbc2b498d8c1f22b36a622d88b907f66c0
Sivahari-gen <sivahari934405@gmail.com>
2026-06-15 09:29:23 +0530

    week 1 completed

---
+++ loop.py
@@ -0,0 +1,12 @@
+#Mulitiplication table
+numb = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{numb} x {i} = {numb * i}")
+
+#sum
+total = 0
+for i in range(1, 101):
+    total += i
+print("total: ",total)
+
+
